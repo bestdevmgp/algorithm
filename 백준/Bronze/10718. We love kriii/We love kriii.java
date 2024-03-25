5767f5a233ba6bb8ae10878b4107d28c56a3b38c
박민규 <147812403+min9park@users.noreply.github.com>
2024-03-25 10:46:14 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 0; i < 2; i++){
+            System.out.println("강한친구 대한육군");
+        }
+    }
+}
