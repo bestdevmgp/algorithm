9e80ef8c77b7021b3593d9d50a5f97aee438252b
박민규 <147812403+min9park@users.noreply.github.com>
2024-03-25 10:44:34 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer A = scanner.nextInt();
+        Integer B = scanner.nextInt();
+        System.out.println((double) A/B);
+    }
+}
