7b16794506601cb5b1a1ea01e7dffba39e994c0c
박민규 <147812403+min9park@users.noreply.github.com>
2024-03-28 20:56:30 +0900

    [Bronze V] Title: A+B - 4, Time: 236 ms, Memory: 17936 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,19 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            while(true){
+                int A = sc.nextInt();
+                int B = sc.nextInt();
+                System.out.println(A+B);
+            }
+        }
+        catch (Exception exception){
+
+        }
+    }
+}
