63425d52d65fb1c25fc4ba3dde9e1bfde52d303d
박민규 <147812403+min9park@users.noreply.github.com>
2024-04-01 20:48:21 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        System.out.println(n + "??!");
+    }
+}
