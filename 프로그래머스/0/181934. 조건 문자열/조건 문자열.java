fe9bc18532bb24aad03ecbe9f185d8a60a5054fd
박민규 <im@parkmingyu.com>
2025-05-13 11:54:19 +0900

    [level 0] Title: 조건 문자열, Time: 1.78 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        String condition = ineq + eq;
+
+        switch (condition) {
+            case ">=":
+                return n >= m ? 1 : 0;
+            case "<=":
+                return n <= m ? 1 : 0;
+            case ">!":
+                return n > m ? 1 : 0;
+            case "<!":
+                return n < m ? 1 : 0;
+            default:
+                return 0;
+        }
+    }
+}
