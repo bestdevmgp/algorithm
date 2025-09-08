778812929d78cd456aeb3c9c97ee0c050a3ea1e9
박민규 <me@mingyu.dev>
2025-09-08 17:05:53 +0900

    [Silver IV] Title: 제로, Time: 2460 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,10 +1,10 @@
-k = int(input())
+n = int(input())
 l = []
-for i in range(k):
-    n = int(input())
-    if n != 0:
-        l.append(n)
-    elif n == 0:
-        del l[-1]
-sums = sum(l)
-print(sums)
+for _ in range(n):
+    a = int(input())
+    if (a != 0):
+        l.append(a)
+    else:
+        if l:
+            l.pop()
+print(sum(l))
